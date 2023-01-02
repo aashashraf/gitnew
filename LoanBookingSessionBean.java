7b930aeb2f0a53be74c141e868bd51a3e815eb86
aashashraf <mohamedashrafali.j@gmail.com>
2023-01-02 09:55:10 +0000

    2nd commit

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -12,7 +12,6 @@ int size = 2 * n - 1;
 //inner loop  
 for(int i = 1; i <= size; i++)  
 {  
-//outer loop      
 for(int j = 1; j <= size; j++)  
 {  
 //calculates and prints the values for pattern  
